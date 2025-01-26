299e9a231fa33d0e035166e5131cc68c587c3292
Byte <coding101x@gmail.com>
2025-01-26 13:42:42 +0700

    addition

--- README.md
+++ README.md
@@ -9,3 +9,8 @@
 **Script:** `translate_title`
 
 **Description:** Auto translate the title of the video playing on MPV.
+
+### Send to YT-DLP
+**Script:** `send_to_ytdlp`
+
+**Description:** Download the currently playing video by sending it to yt-dlp
---
+++ send_to_ytdlp.js
@@ -0,0 +1,56 @@
+/**
+ * https://github.com/BitByteBits/mpv-scripts
+ *
+ * Download the currently playing video using yt-dlp. Using CTRL+ALT+D shortcut by default.
+ * Make sure yt-dlp is in %PATH% and set all the options in yt-dlp.conf then:
+ *
+ * - Adjust dest to your download directory
+ * - For custom shortcut add: KEY script-binding send_to_ytdlp
+ * - If using UOSC find controls=menu in uosc.conf and add this inside: <video>command:file_download:script-binding send_to_ytdlp?Download
+ *
+ */
+
+var dest = "J:/Youtube/Music";
+
+function displayOverlay(text) {
+	var a = mp.create_osd_overlay("ass-events");
+	a.data = "{\\an2\\bord10\\1c&HFFFFFF&\\3c&H000000&}" + text;
+	a.update();
+	setTimeout(function() {
+		a.remove();
+	}, mp.get_property("osd-duration"));
+}
+
+function sendToYTDLP() {
+	var path = mp.get_property("path");
+		dest = mp.command_native(["normalize-path", dest]);
+
+	if (!path.match(/^https?:\//)) {
+		displayOverlay("Invalid URL");
+		return;
+	}
+
+	displayOverlay("Downloading " + path);
+	var args = ["yt-dlp.exe", "--paths", dest, path];
+	mp.command_native_async({
+		name: "subprocess",
+		playback_only: false,
+		capture_stderr: true,
+		capture_stdout: true,
+		args: args,
+	},
+		function (success, result, error) {
+		if (success) {
+			if (result.status == 0) {
+				displayOverlay("Downloaded to " + dest);
+				print(result.stdout);
+			} else {
+				displayOverlay("Something went wrong");
+			}
+		} else {
+			mp.msg.error(error);
+		}
+	});
+}
+
+mp.add_key_binding("ctrl+alt+d", "send_to_ytdlp", sendToYTDLP);
