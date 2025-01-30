a3df4f449814ac3f7746b4e32faf15dd35a4a876
Byte <coding101x@gmail.com>
2025-01-30 16:56:37 +0700

    allow override

--- send_to_ytdlp.js
+++ send_to_ytdlp.js
@@ -4,13 +4,17 @@
  * Download the currently playing video using yt-dlp. Using CTRL+ALT+D shortcut by default.
  * Make sure yt-dlp is in %PATH% and set all the options in yt-dlp.conf then:
  *
- * - Adjust dest to your download directory (by default it'll download to your mpv directory)
+ * - Adjust dest to your download directory (by default it'll download to your mpv directory) or by setting
+ *   script-opts-append=send_to_ytdlp-destination=X:/Destination
  * - For custom shortcut add: KEY script-binding send_to_ytdlp
  * - If using UOSC find controls=menu in uosc.conf and add this inside: <video>command:file_download:script-binding send_to_ytdlp?Download
  *
  */
 
-var destination = "~~/";
+var o = {
+	destination: "~~/"
+}
+mp.options.read_options(o);
 
 function displayOverlay(text) {
 	var a = mp.create_osd_overlay("ass-events");
@@ -23,16 +27,17 @@ function displayOverlay(text) {
 
 function sendToYTDLP() {
 	var path = mp.get_property("path");
-		destination = mp.command_native(["expand-path", destination]);
-		destination = mp.command_native(["normalize-path", destination]);
+	var dest = o.destination;
+		dest = mp.command_native(["expand-path", dest]);
+		dest = mp.command_native(["normalize-path", dest]);
 
 	if (!path.match(/^https?:\//)) {
 		displayOverlay("Invalid URL");
 		return;
 	}
 
-	displayOverlay("Downloading " + path + " to " + destination);
-	var args = ["yt-dlp.exe", "--paths", destination, path];
+	displayOverlay("Downloading " + path + " to " + dest);
+	var args = ["yt-dlp.exe", "--paths", dest, path];
 	mp.command_native_async({
 		name: "subprocess",
 		playback_only: false,
@@ -43,7 +48,7 @@ function sendToYTDLP() {
 		function (success, result, error) {
 		if (success) {
 			if (result.status == 0) {
-				displayOverlay("Downloaded to " + destination);
+				displayOverlay("Downloaded to " + dest);
 				print(result.stdout);
 			} else {
 				displayOverlay("Something went wrong");
